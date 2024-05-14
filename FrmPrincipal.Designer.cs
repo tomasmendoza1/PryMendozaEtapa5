588eccc02decaed08dbcc3556c380e83c2e60ea1
Alumnos <Alumnos@PC143>
2024-05-14 12:22:04 -0300

    Agregar archivos de proyecto.

---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace PryMendozaEtapa5
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCrear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnCrear
+            // 
+            this.BtnCrear.Location = new System.Drawing.Point(12, 209);
+            this.BtnCrear.Name = "BtnCrear";
+            this.BtnCrear.Size = new System.Drawing.Size(391, 44);
+            this.BtnCrear.TabIndex = 0;
+            this.BtnCrear.Text = "Generar imagen aleatoria ";
+            this.BtnCrear.UseVisualStyleBackColor = true;
+            this.BtnCrear.Click += new System.EventHandler(this.BtnCrear_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(415, 265);
+            this.Controls.Add(this.BtnCrear);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Etapa 5 - CREAR OBJETOS POR CÓDIGO - ALEATORIO";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCrear;
+    }
+}
+
---
+++ FrmPrincipal.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryMendozaEtapa5
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+        clsVehiculo objAuto; clsVehiculo objAvion; clsVehiculo objBarco;
+        bool Auto; bool Barco; bool Avion;
+        int GeneradorDeImagen;
+        private void BtnCrear_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            GeneradorDeImagen = rnd.Next(1,4);
+            switch(GeneradorDeImagen)
+            {
+                case 1:
+                    objAuto.CrearAuto();
+                    objAuto.pctAuto.Location = new Point(70, 1);
+                    Controls.Add(objAuto.pctAuto);
+                    Auto = true;
+                    if (Avion == true)
+                    {
+                        objAvion.pctAvion.Dispose();
+                        Avion = false;
+                    }
+                    if (Barco == true)
+                    {
+                        objBarco.pctBarco.Dispose();
+                        Barco = false;
+                    }
+                break;
+
+                case 2:
+                    objAvion.CrearAvion();
+                    objAvion.pctAvion.Location = new Point(70, 1);
+                    Controls.Add(objAvion.pctAvion);
+                    Avion = true;
+                    if (Auto == true)
+                    {
+                        objAuto.pctAuto.Dispose();
+                        Auto = false;
+                    }
+                    if (Barco == true)
+                    {
+                        objBarco.pctBarco.Dispose();
+                        Barco = false;
+                    }
+                break;
+
+                case 3:
+                    objBarco.CrearBarco();
+                    objBarco.pctBarco.Location = new Point(70, 1);
+                    Controls.Add(objBarco.pctBarco);
+                    Barco = true;
+                    if (Auto == true)
+                    {
+                        objAuto.pctAuto.Dispose();
+                        Auto = false;
+                    }
+                    if (Avion == true)
+                    {
+                        objAvion.pctAvion.Dispose();
+                        Avion = false;
+                    }
+                break;
+            }
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            objAuto = new clsVehiculo();
+            objAvion = new clsVehiculo();
+            objBarco = new clsVehiculo();
+            objAuto.CrearAuto();
+            objAvion.CrearAvion();
+            objBarco.CrearBarco();
+        }
+    }
+}
